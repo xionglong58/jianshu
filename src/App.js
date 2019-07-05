0d84b76afff2a65acc01202e5bd7a6b964127711
xionglong58 <xionglong58@email.swu.edu.cn>
2019-07-05 20:13:23 +0800

    react-router-dom基础用法

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React, { Fragment } from "react";
 import { GlobalStyle } from "./style";
 import Header from "./common/header/index";
 import { GlobalIconfont } from "./static/iconfont/iconfont";
-import store from './store/index'
-import {Provider} from 'react-redux'
+import store from './store/index';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 const App = () => {
   return (
     <Fragment>
       <GlobalStyle />
       <GlobalIconfont />
       <Provider store={store}>
-      <Header />
-      <div style={{height:'100px',backgroundColor: '#008080'}}>footer</div>
+        <Fragment>
+          <Header />
+          <BrowserRouter>
+            <Route path="/" exact render={() => <div>HOME</div>}></Route>
+            <Route path="/route1" exact render={() => <div>Route1</div>}></Route>
+          </BrowserRouter>
+          <div style={{ height: '100px', backgroundColor: '#008080' }}>footer</div>
+        </Fragment>
       </Provider>
-    </Fragment>
+    </Fragment >
   );
 };
 
