4ea66ffe08db6b501954803ba7428a445ec22383
xionglong58 <xionglong58@email.swu.edu.cn>
2019-07-16 21:02:55 +0800

    Updates

---
+++ precache-manifest.7303f7795ce9a1c7e9c3a180d80a5a71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba15e441de07af53c47e70317557d58",
+    "url": "/jianshu/index.html"
+  },
+  {
+    "revision": "34f78cdc59faa3e4b16d",
+    "url": "/jianshu/static/js/2.4306133e.chunk.js"
+  },
+  {
+    "revision": "78fabb0b21c393374ea4",
+    "url": "/jianshu/static/js/3.d0b83c9a.chunk.js"
+  },
+  {
+    "revision": "1f930a157fd94b323fcd",
+    "url": "/jianshu/static/js/main.8fb32733.chunk.js"
+  },
+  {
+    "revision": "cd497616c0123aac9347",
+    "url": "/jianshu/static/js/runtime~main.96c7650f.js"
+  }
+]);
